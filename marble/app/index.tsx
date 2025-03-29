b2f684e16b1cdd42aa025a25e93e009e0f717969
Kudakwashe Nyengera <kuda@fluff.software>
2025-03-29 11:59:55 +0200

    setup expo router

---
+++ marble/app/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View>
+      <Text style={{ fontSize: 20, fontWeight: "bold" }}>Pantry by Marble</Text>
+    </View>
+  );
+}
