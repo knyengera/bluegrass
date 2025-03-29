ceeb11b405b06bb5b719bdcc89e777659dae8437
Kudakwashe Nyengera <kuda@fluff.software>
2025-03-29 13:28:39 +0200

    added gluestack ui

--- marble/app/_layout.tsx
+++ marble/app/_layout.tsx
@@ -1,11 +1,16 @@
 import { Stack } from "expo-router";
 
+import "@/global.css";
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
+
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" />
-      <Stack.Screen name="cart" />
-      <Stack.Screen name="products" />
-    </Stack>
+    <GluestackUIProvider>
+      <Stack>
+        <Stack.Screen name="index" />
+        <Stack.Screen name="cart" />
+        <Stack.Screen name="products" />
+      </Stack>
+    </GluestackUIProvider>
   );
 }
