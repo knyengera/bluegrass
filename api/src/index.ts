269b976540971599f639e856a69fc64a04110b70
Kudakwashe Nyengera <kuda@fluff.software>
2025-03-29 19:27:46 +0200

    added json middleware

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,16 +1,20 @@
-import express from "express";
+import express, { json, urlencoded } from "express";
 import productsRoutes from "./routes/products";
 import ordersRoutes from "./routes/orders";
 
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-    res.send("Hello World");
-});
+app.use(urlencoded({ extended: false }));
+app.use(json());
 
+// Routes
 app.use('/products', productsRoutes);
 app.use('/orders', ordersRoutes);
+
+app.get("/", (req, res) => {
+    res.send("Pantry by Marble Api");
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
