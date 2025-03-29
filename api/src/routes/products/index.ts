c50267db349b0fa20aaf2e666c167276fe32252e
Kudakwashe Nyengera <kuda@fluff.software>
2025-03-29 23:36:44 +0200

    implimented products controller

--- api/src/routes/products/index.ts
+++ api/src/routes/products/index.ts
@@ -1,10 +1,20 @@
 import {Router} from "express";
-import { getAllProducts, getProductById, createProduct, updateProduct, deleteProduct } from "./productsController";
+import { 
+    getAllProducts, 
+    getProductById, 
+    createProduct, 
+    updateProduct, 
+    deleteProduct, 
+    getProductByName, 
+    getProductByCategory 
+} from "./productsController";
 
 const router = Router();
 
 router.get('/', getAllProducts);
 router.get('/:id', getProductById);
+router.get('/name/:name', getProductByName);
+router.get('/category/:category', getProductByCategory);
 router.post('/', createProduct);
 router.put('/:id', updateProduct);
 router.delete('/:id', deleteProduct);
--- api/src/routes/products/productsController.ts
+++ api/src/routes/products/productsController.ts
@@ -1,21 +1,95 @@
 import { Request, Response } from "express";
+import { db } from "../../db";
+import { productsTable } from "../../db/productsSchema";
+import { eq, like } from "drizzle-orm";
 
-export function getAllProducts (req: Request, res: Response) {
-    res.send("The List of Products");
+export async function getAllProducts (req: Request, res: Response) {
+    try {
+        const products = await db.select().from(productsTable);
+        res.status(200).json(products);
+    } catch (error) {
+        res.status(500).json({ error: "Failed to get products" });
+    }
 };
 
-export function getProductById (req: Request, res: Response) {
-    res.send(`Product ID is ${req.params.id}`);
+export async function getProductById (req: Request, res: Response) {
+    try {
+        const [product] = await db.select().from(productsTable).where(eq(productsTable.id, parseInt(req.params.id)));
+        if (!product) {
+            res.status(404).json({ error: "Product not found" });
+        } else {
+            res.status(200).json(product);
+        }
+    } catch (error) {
+        res.status(500).json({ error: "Failed to get product" });
+    }
 };
 
-export function createProduct (req: Request, res: Response) {
-    res.send("Creating a new product");
-};  
-
-export function updateProduct (req: Request, res: Response) {
-    res.send(`Updating product ${req.params.id}`);
+export async function getProductByName (req: Request, res: Response) {
+    try {
+        const products = await db.select().from(productsTable).where(like(productsTable.name, `%${req.params.name}%`));
+        if (!products.length) {
+            res.status(404).json({ error: "Product not found" });
+        } else {
+            res.status(200).json(products);
+        }
+    } catch (error) {
+        res.status(500).json({ error: "Failed to get product" });
+    }
 };
 
-export function deleteProduct (req: Request, res: Response) {
-    res.send(`Deleting product ${req.params.id}`);
+export async function getProductByCategory (req: Request, res: Response) {
+    try {
+        const products = await db.select().from(productsTable).where(like(productsTable.mainCategory, `%${req.params.category}%`));
+        if (!products.length) {
+            res.status(404).json({ error: "Product not found" });
+        } else {
+            res.status(200).json(products);
+        }
+    } catch (error) {
+        res.status(500).json({ error: "Failed to get product" });
+    }
+};
+
+export async function createProduct (req: Request, res: Response) {
+    try {
+        const newProduct = req.body;
+        const product = await db.insert(productsTable).values({
+            ...newProduct,
+            createdAt: new Date(),
+        }).returning(); 
+        res.status(201).json(product);
+        } catch (error) {
+            res.status(500).json({ error: "Failed to create product" });
+        }
+};
+
+export async function updateProduct (req: Request, res: Response) {
+    try {
+        const updatedProduct = req.body;
+        const [product] = await db.update(productsTable).set({
+            ...updatedProduct,
+            updatedAt: new Date(),
+        }).where(eq(productsTable.id, parseInt(req.params.id))).returning();
+        if (!product) {
+            res.status(404).json({ error: "Product not found" });
+        } else {
+            res.status(200).json(product);
+        }
+    } catch (error) {
+        res.status(500).json({ error: "Failed to update product" });
+    }
+};
+
+export async function deleteProduct (req: Request, res: Response) {
+    try {
+        const [product] = await db.delete(productsTable).where(eq(productsTable.id, parseInt(req.params.id))).returning();
+        if (!product) {
+            res.status(404).json({ error: "Product not found" });
+        } else {
+            res.status(204).send();
+        }
+    } catch (error) {
+        res.status(500).json({ error: "Failed to delete product" });
+    }
 };
