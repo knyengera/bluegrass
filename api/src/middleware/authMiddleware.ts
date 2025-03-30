dd1788c31ae7f329204b1f91c34f41d83e355c4c
Kudakwashe Nyengera <kuda@fluff.software>
2025-03-30 18:42:22 +0200

    clean up middleware to make it simpler

--- api/src/middleware/authMiddleware.ts
+++ api/src/middleware/authMiddleware.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import jwt from 'jsonwebtoken';
+
 const JWT_SECRET = process.env.JWT_SECRET;
 
 export function authenticateToken(req: Request, res: Response, next: NextFunction) {
@@ -15,6 +16,7 @@ export function authenticateToken(req: Request, res: Response, next: NextFunctio
             return;
         }
         req.userId = decoded.userId;
+        req.role = decoded.role;
         next();
     } catch (error) {
         res.status(401).json({ message: 'Unauthorized' });
@@ -34,6 +36,7 @@ export function authenticateRefreshToken(req: Request, res: Response, next: Next
             return;
         }
         req.userId = decoded.userId;
+        req.role = decoded.role;
         next();
     } catch (error) {
         res.status(401).json({ message: 'Unauthorized' });
@@ -41,24 +44,9 @@ export function authenticateRefreshToken(req: Request, res: Response, next: Next
 }
 
 export function verifyAdmin(req: Request, res: Response, next: NextFunction) {
-    const token = req.headers.authorization;
-    if (!token) {
-        res.status(401).json({ message: 'Unauthorized' });
+    if (req.role !== 'admin') {
+        res.status(401).json({ message: 'User not authorised to perform this action!' });
         return;
     }
-    try {
-        const decoded = jwt.verify(token, JWT_SECRET as string);
-        if (typeof decoded != 'object' || !decoded?.userId) {
-            res.status(401).json({ message: 'Unauthorized' });
-            return;
-        }
-        if (decoded.role != 'admin' ) {
-            res.status(401).json({ message: 'User not authorised to perform this action!' });
-            return;
-        }
-        req.userId = decoded.userId;
-        next();
-    } catch (error) {
-        res.status(401).json({ message: 'Unauthorized' });
-    }
+    next();
 }
